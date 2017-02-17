9cdf239b0ab3f684c5be56cb60dc82625400af55
Hao Wu <hao.a.wu@intel.com>
2017-02-17 11:00:45 +0800

    ShellPkg: Refine casting expression result to bigger size
    There are cases that the operands of an expression are all with rank less
    than UINT64/INT64 and the result of the expression is explicitly cast to
    UINT64/INT64 to fit the target size.
    
    An example will be:
    UINT32 a,b;
    // a and b can be any unsigned int type with rank less than UINT64, like
    // UINT8, UINT16, etc.
    UINT64 c;
    c = (UINT64) (a + b);
    
    Some static code checkers may warn that the expression result might
    overflow within the rank of "int" (integer promotions) and the result is
    then cast to a bigger size.
    
    The commit refines codes by the following rules:
    1). When the expression is possible to overflow the range of unsigned int/
    int:
    c = (UINT64)a + b;
    
    2). When the expression will not overflow within the rank of "int", remove
    the explicit type casts:
    c = a + b;
    
    3). When the expression will be cast to pointer of possible greater size:
    UINT32 a,b;
    VOID *c;
    c = (VOID *)(UINTN)(a + b); --> c = (VOID *)((UINTN)a + b);
    
    4). When one side of a comparison expression contains only operands with
    rank less than UINT32:
    UINT8 a;
    UINT16 b;
    UINTN c;
    if ((UINTN)(a + b) > c) {...} --> if (((UINT32)a + b) > c) {...}
    
    For rule 4), if we remove the 'UINTN' type cast like:
    if (a + b > c) {...}
    The VS compiler will complain with warning C4018 (signed/unsigned
    mismatch, level 3 warning) due to promoting 'a + b' to type 'int'.
    
    Cc: Jaben Carsey <jaben.carsey@intel.com>
    Cc: Ruiyu Ni <ruiyu.ni@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Hao Wu <hao.a.wu@intel.com>

--- ShellPkg/Library/UefiShellDebug1CommandsLib/UefiShellDebug1CommandsLib.c
+++ ShellPkg/Library/UefiShellDebug1CommandsLib/UefiShellDebug1CommandsLib.c
@@ -1,7 +1,7 @@
 /** @file
   Main file for NULL named library for debug1 profile shell command functions.
 
-  Copyright (c) 2010 - 2011, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) 2010 - 2017, Intel Corporation. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -193,7 +193,7 @@ HexCharToUintn (
     return Char - L'0';
   }
 
-  return (UINTN) (10 + CharToUpper (Char) - L'A');
+  return (10 + CharToUpper (Char) - L'A');
 }
 
 /**
