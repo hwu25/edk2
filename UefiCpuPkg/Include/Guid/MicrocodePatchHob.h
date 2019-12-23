369ce6313d9d42b666ceba39b789774fd1a4aef7
Hao A Wu <hao.a.wu@intel.com>
2019-12-23 10:52:14 +0800

    UefiCpuPkg: Add definitions for EDKII microcode patch HOB
    REF:https://bugzilla.tianocore.org/show_bug.cgi?id=2430
    
    This commit will add the definitions for EDKII microcode patch HOB.
    
    The intention of adding this HOB is to provide a scheme to store the below
    information:
    
    A. The base address and size of the microcode patches that are being
       loaded (from flash) into memory;
    B. The information of applied microcode patch for each processor within
       the system.
    
    The producer of the HOB will be the UefiCpuPkg/MpInitLib (where the load,
    detect and apply of the microcode happen). The consumer of the HOB can be
    modules that want to detect/apply the microcode patch by themselves again
    later during the boot flow.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Siyuan Fu <siyuan.fu@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Signed-off-by: Hao A Wu <hao.a.wu@intel.com>

---
+++ UefiCpuPkg/Include/Guid/MicrocodePatchHob.h
@@ -0,0 +1,50 @@
+/** @file
+  The microcode patch HOB is used to store the information of:
+    A. Base address and size of the loaded microcode patches data;
+    B. Applied microcode patch for each processor within system.
+
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php.
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _MICROCODE_PATCH_HOB_H_
+#define _MICROCODE_PATCH_HOB_H_
+
+extern EFI_GUID gEdkiiMicrocodePatchHobGuid;
+
+//
+// The EDKII microcode patch HOB will be produced by MpInitLib and it can be
+// consumed by modules that want to detect/apply microcode patches.
+//
+typedef struct {
+  //
+  // The base address of the microcode patches data after being loaded into
+  // memory.
+  //
+  UINT64    MicrocodePatchAddress;
+  //
+  // The total size of the loaded microcode patches.
+  //
+  UINT64    MicrocodePatchRegionSize;
+  //
+  // The number of processors within the system.
+  //
+  UINT32    ProcessorNumber;
+  //
+  // An array with 'ProcessorNumber' elements that stores the offset (with
+  // regard to 'MicrocodePatchAddress') of the applied microcode patch for each
+  // processor.
+  // If no microcode patch is applied for certain processor, the relating
+  // element will be set to MAX_UINT64.
+  //
+  UINT64    DetectedPatchOffset[0];
+} EDKII_MICROCODE_PATCH_HOB;
+
+#endif
